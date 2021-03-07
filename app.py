c569b9ea4bf6a0c65a5175dc38c70a9acd964b57
ILIAS IBRAHIM <iliasone@yahoo.com>
2021-03-08 01:41:45 +1100

    update app.py

--- app.py
+++ app.py
@@ -18,11 +18,11 @@ local_css("style.css")
 with st.sidebar:
     st.subheader('Enter Stack Overflow User ID:')
     userId = st.text_input('')
-    if userId.isalpha():
+    if not userId.isdigit():
         st.error("User ID must be digits only!")
     
 # Code for the profile info display page
-if userId and (userId.isalpha() == False):
+if userId and userId.isdigit():
     BASEURL = "https://api.stackexchange.com/2.2/users/"+userId
     params = {
     "site" : "stackoverflow"
