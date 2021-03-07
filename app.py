ef57539c340efb87dda246069455ab1ed711b18a
ILIAS IBRAHIM <iliasone@yahoo.com>
2021-03-08 00:23:43 +1100

    add message when user is not found

--- app.py
+++ app.py
@@ -30,37 +30,41 @@ if userId:
     resp = requests.get(BASEURL, params=params)
     user_details = resp.json()
 
-    #Scrape user's rating directly
-    rank_link = 'https://stackoverflow.com/users/rank?userId='+userId
-    profile_page = requests.get(rank_link)
-    soup = BeautifulSoup(profile_page.content, 'html.parser')
-    
-    #assign user details to variables
-    display_name = user_details['items'][0]['display_name']
-    reputation = user_details['items'][0]['reputation']
-    image_url = user_details['items'][0]['profile_image']
-    badges = user_details['items'][0]['badge_counts']
-    gold = badges['gold']
-    silver = badges['silver']
-    bronze = badges['bronze']
+    if len(user_details['items']) > 0:
 
-    #check if user is ranked
-    if soup.find('b'):
-        percentile = soup.find('b').get_text()
-    else :
-        percentile = '0%'
+        #Scrape user's rating directly
+        rank_link = 'https://stackoverflow.com/users/rank?userId='+userId
+        profile_page = requests.get(rank_link)
+        soup = BeautifulSoup(profile_page.content, 'html.parser')
+        
+        #assign user details to variables
+        display_name = user_details['items'][0]['display_name']
+        reputation = user_details['items'][0]['reputation']
+        image_url = user_details['items'][0]['profile_image']
+        badges = user_details['items'][0]['badge_counts']
+        gold = badges['gold']
+        silver = badges['silver']
+        bronze = badges['bronze']
 
-    #Display user details in main page
-    st.image(image_url)
-    st.markdown(f'<p class="big-font"> Name: {display_name}</p>', unsafe_allow_html=True)
-    st.markdown(f'<p class="big-font"> Reputation: {reputation}</p>', unsafe_allow_html=True)
-    st.markdown(f'<p class="big-font"> Badges: </p>', unsafe_allow_html=True)
-    st.markdown(f'''
-                * <b id="gold">Gold: {gold}</b>
-                * <b id="silver">Silver: {silver}</b>
-                * <b id="bronze">Bronze: {bronze}</b>
-                ''', unsafe_allow_html=True)
-    st.markdown(f'<p class="big-font" id="top"> Top: {percentile} Overall</p>', unsafe_allow_html=True)
+        #check if user is ranked
+        if soup.find('b'):
+            percentile = soup.find('b').get_text()
+        else :
+            percentile = '0%'
+
+        #Display user details in main page
+        st.image(image_url)
+        st.markdown(f'<p class="big-font"> Name: {display_name}</p>', unsafe_allow_html=True)
+        st.markdown(f'<p class="big-font"> Reputation: {reputation}</p>', unsafe_allow_html=True)
+        st.markdown(f'<p class="big-font"> Badges: </p>', unsafe_allow_html=True)
+        st.markdown(f'''
+                    * <b id="gold">Gold: {gold}</b>
+                    * <b id="silver">Silver: {silver}</b>
+                    * <b id="bronze">Bronze: {bronze}</b>
+                    ''', unsafe_allow_html=True)
+        st.markdown(f'<p class="big-font" id="top"> Top: {percentile} Overall</p>', unsafe_allow_html=True)
+    else: 
+        st.info("A profile with that User ID is not found!")
     
 else :
     st.info("Please enter Stackoverflow UserId in the left input box")
